6c3881260d6e036911017c0a9a467ed75915400a
puhrome <phirom.yim@fullsail.edu>
2014-05-02 20:23:50 -0400

    Function If Else
    Using If Else and While to create a function

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/**
+ * Testing out If Else and Function Statements Created by puhrome on 5/1/14.
+ */
+var eggHatch = Number(prompt("Enter your age"));
+if (eggHatch > 0 && eggHatch <= 20){
+    console.log("Sorry guys, you're still an egg")
+}else if ((eggHatch > 0 && eggHatch <= 2)|| (eggHatch >= 21 && eggHatch <=23)){
+    console.log("Welcome to the world, little chickadees!")
+}
